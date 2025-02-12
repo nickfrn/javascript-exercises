d6f82f2a2fcccf1bae202cd4447ebc6000861c7e
nickfrn <193815257+nickfrn@users.noreply.github.com>
2025-02-12 19:25:15 +0100

    Complete exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return 'ERROR';
+    } else {
+        newString = '';
 
+        for (let i = 0; i < num; i++) {
+            newString += string;
+        }
+
+        return newString;
+    }
 };
 
 // Do not edit below this line
