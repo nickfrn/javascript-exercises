722cc42cb61e5ea0a131534ae700c3803fe6b342
nickfrn <193815257+nickfrn@users.noreply.github.com>
2025-02-12 21:27:52 +0100

    Complete exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let newArray = [];
+
+    for (element of array) {
+        if (!args.includes(element)) {
+            newArray.push(element);
+        }
+    }
+
+    return newArray;
 };
 
 // Do not edit below this line
