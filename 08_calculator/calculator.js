aa3e3fc698808c0dbfb70230ea35f033c4d8b296
nickfrn <193815257+nickfrn@users.noreply.github.com>
2025-02-18 20:36:33 +0100

    Complete exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numsArr) {
+    if (numsArr.length < 1) {
+        return 0;
+    } else {
+      const numsSum = numsArr.reduce((total, currentNum) => {
+        return total += currentNum;
+      });
+
+      return numsSum;
+    }
 };
 
-const multiply = function() {
+const multiply = function(numsArr) {
+  const numsProduct = numsArr.reduce((total, currentNum) => {
+    return total * currentNum;
+  });
 
+  return numsProduct;
 };
 
-const power = function() {
-	
-};
+const power = function(num1, num2) {
+    return num1 ** num2;
+}
 
-const factorial = function() {
-	
+const factorial = function(num) {
+    let result = 1;
+
+    if (num == 0) {
+        return result;
+    } else {
+        for (let i = num; i > 0; i--) {
+          result *= i;
+        }
+
+        return result;
+    }
 };
 
 // Do not edit below this line
