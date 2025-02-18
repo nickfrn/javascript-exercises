e0f6ea2caf702171a0805766e372a3b8834621e1
nickfrn <193815257+nickfrn@users.noreply.github.com>
2025-02-18 21:06:30 +0100

    Complete exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    let cleanString = string
+        .toLowerCase()
+        .split('')
+        .filter(char => alphanumerical.includes(char))
+        .join('');
+
+    let reversedString = cleanString.split('').reverse().join('');
+
+    return reversedString === cleanString;
 };
 
 // Do not edit below this line
