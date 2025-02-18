7e154b0b0b74e8f8ab6b9b115d59c7f742499852
nickfrn <193815257+nickfrn@users.noreply.github.com>
2025-02-18 21:17:53 +0100

    Complete exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    let sequence = [0, 1];
 
+    if (+index < 0) {
+        return 'OOPS';
+    } else {
+        for(let i = 2; i <= index; i++) {
+            sequence[i] = sequence[i - 2] + sequence[i - 1];
+        }
+
+        return sequence[index];
+    }
 };
 
 // Do not edit below this line
