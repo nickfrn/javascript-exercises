03002173c51e31b0cad6bc7498c221d6608b8439
nickfrn <193815257+nickfrn@users.noreply.github.com>
2025-02-19 13:26:41 +0100

    Complete exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currentDate = new Date();
+    const currentYear =  currentDate.getFullYear();
 
+    let mappedPeople = people
+    .map(person => {
+        if ('yearOfDeath' in person) {
+            person.age = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            person.age = currentYear - person.yearOfBirth;
+        }
+
+        return person;
+    });
+
+    let oldestPerson = people[0];
+
+    let oldest = mappedPeople.reduce((oldestPerson, person) => {
+        if (person.age > oldestPerson.age) return oldestPerson = person;
+
+        return oldestPerson;
+    });
+
+    return oldest;
 };
 
 // Do not edit below this line
